4ba7538d63876bcc85a38047b0f29def4ee2a85c
jgarciagimenez <josemanuelgarciagimenez@gmail.com>
2016-02-27 17:25:49 +0100

    Añadido Generación y almacenamiento de claves
    Script para generar un par de claves RSA serializarlos y almacenarlos en
    archivos binarios.

---
+++ generarClaves/generarRSA.py
@@ -0,0 +1,35 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+private_key = rsa.generate_private_key(
+    public_exponent = 65537,
+    key_size = 1024,
+    backend = default_backend()
+)
+public_key = private_key.public_key()
+
+## Serialización de la clave privada
+
+private_key_ser = private_key.private_bytes(
+    encoding = serialization.Encoding.PEM,
+    format = serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm = serialization.NoEncryption()
+)
+
+## Serialización de la clave pública
+
+public_key_ser = public_key.public_bytes(
+	encoding = serialization.Encoding.PEM,
+	format = serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+
+outfile = open('clave_privada.pem','wb')
+outfile.write(private_key_ser)
+outfile.close
+
+outfile = open('clave_publica.pem','wb')
+outfile.write(public_key_ser)
+outfile.close
+

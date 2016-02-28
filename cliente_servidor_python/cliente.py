3fff638c3e0eaaa80b18035a6ed9e022d173240b
jgarciagimenez <josemanuelgarciagimenez@gmail.com>
2016-02-28 12:38:21 +0100

    Añadido cifrado y descifrado al cliente y servidor existentes
    Basándome en la prueba de cifrado del commit anterior, he añadido
    cifrado y descifrado en la comunicación cliente servidor con
    codificación RSA, las claves están guardadas en los archivos binarios
    .pem

--- cliente_servidor_python/cliente.py
+++ cliente_servidor_python/cliente.py
@@ -1,6 +1,11 @@
 import socket
 import time
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
 ## Definimos lo parámetros para el socket , se podrían ingresar por la consola si se desea
 
 udpIP = "127.0.0.1"
@@ -10,14 +15,34 @@ udpPORT = 4444
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
+## Cargamos la clave pública de un fichero .pem
+
+with open ('clave_publica.pem','rb') as key_file:
+	public_key = serialization.load_pem_public_key(
+		key_file.read(),
+		backend = default_backend()
+	)
+
 
 ## Pasamos al bucle donde se transmiten datos periodicamente
 
 while True :
 
-#	mensaje = str.encode('CONTROL sadfasdf')
-    mensaje = str.encode("asdfasd")
-    sock.sendto(mensaje,(udpIP,udpPORT))
-    time.sleep(5)
+			#	mensaje = str.encode('CONTROL sadfasdf')
+	mensaje = str.encode("asdfasd")
+
+				## Ciframos el mensaje con la clave pública
+
+	cifrado = public_key.encrypt(
+		mensaje,
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA1()),
+			algorithm=hashes.SHA1(),
+			label=None
+		)
+	)
+
+	sock.sendto(cifrado,(udpIP,udpPORT))
+	time.sleep(5)
 
 
--- cliente_servidor_python/servidor.py
+++ cliente_servidor_python/servidor.py
@@ -1,43 +1,79 @@
 import socket 
 import time
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
     
 def defcon(codigo):
     ## Función para cambiar los parámetros del nivel de seguridad.
     
     print ("se ha entradoe en la función defcon para cambiar el nivel de seguridad")
     
-   
+def escribeFichero(valor):
+
+    t = time.strftime("%H:%M:%S")
+    d = time.strftime("%d/%m/%Y")   
+    print (str(d) +" "+ str(t) + "  El sensor recive:  " + recibido)
+    outfile.write( "\n" + str(d) +" "+ str(t) + "  El sensor recive: " + recibido)
+    outfile.close
+
+
    
 ## Creamos las variables con los parámetros para el socket y creamos el socket
 
 udpIP = '127.0.0.1'
 udpPORT = 4444
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-estadoEncryp = False
+estadoEncryp = True
 
 ## Iniciamos el socket
 
 sock.bind ((udpIP,udpPORT))
 
+## Cargamos la clave privada para leer los mensajes cifrados.
+
+with open ('clave_privada.pem','rb') as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password = None,
+        backend = default_backend()
+    )
+
+
 ## Creamos un bucle para que esté escuhando constantenmente
 
 while True :
     
-    datos,direccion = sock.recvfrom(1024)  ## 1024 es el tamaño del buffer
-    recibido = datos.decode("UTF-8")
+
+    datos,direccion = sock.recvfrom(1024)  ## 1024 es el tamaño del buffer    
     outfile = open('sensor_recibido.txt','a')
-    
-    if recibido.startswith("CONTROL"):
-        defcon(recibido)        
+       
+    if estadoEncryp is True :   ## Si el texto viene cifrado
+
+        cifrado = datos
+        
+        descifrado = private_key.decrypt(
+            cifrado,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA1()),
+                algorithm=hashes.SHA1(),
+                label=None
+            )
+        )
+        recibido = descifrado.decode('UTF-8')
+
+    else :            ## Si el texto viene sin crifrar
+
+        recibido = datos.decode('UTF-8')
+   
+    if  recibido.startswith("CONTROL"):    ## Si viene un mensaje de control
+        defcon(recibido)
 
     else :
-      
-        t = time.strftime("%H:%M:%S")
-        d = time.strftime("%d/%m/%Y")	
-        print (str(d) +" "+ str(t) + "  El sensor recive:  " + recibido)
-        outfile.write( "\n" + str(d) +" "+ str(t) + "  El sensor recive: " + recibido)
-        outfile.close
-    
+        escribeFichero(recibido)  
+
+
   
 
